e6b65c82c0bde92656d9482dc2f6359d0f3a7c26
ConsuL <consul.developer@gmail.com>
2021-06-22 13:05:09 +0300

    v2

---
+++ app/Http/Controllers/Api/V2/DeskController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
+use App\Http\Resources\DeskResource;
+use App\Models\Desk;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+
+class DeskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return DeskResource::collection(Desk::all());
+    }
+
+    /**
+     * @param DeskStoreRequest $request
+     * @return DeskResource
+     */
+    public function store(DeskStoreRequest $request)
+    {
+        //подключили DeskStoreRequst
+        //используем validated
+        $createdDesk = Desk::create($request->validated());
+        return new DeskResource($createdDesk);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show(Desk $desk)
+    {
+        return new DeskResource($desk);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param DeskStoreRequest $request
+     * @param Desk $desk
+     * @return Desk
+     */
+    public function update(DeskStoreRequest $request, Desk $desk)
+    {
+        $desk->update($request->validated());
+
+        return new DeskResource($desk);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy(Desk $desk)
+    {
+        $desk->delete();
+
+        return \response(null, Response::HTTP_NO_CONTENT);
+    }
+}
